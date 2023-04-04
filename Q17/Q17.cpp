0f0e0860c62b71c4f976df16c0deffa452cf344d
GuvuYo <yoyo241357@gmail.com>
2023-04-04 23:07:59 +0800

    15~24

---
+++ Q17/Q17.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+int main(){
+    int i=0,og=0;
+    string txt;
+    string dir;
+    string dir1[20];
+    getline(cin,txt);
+    istringstream ss(txt);
+    while (getline(ss,dir,' '))
+    {
+        if(dir[0]<'Z'){
+            dir[0]=dir[0]+32;
+        }
+        dir1[i]=dir;
+        i++;
+    }
+    for (int j = 0; j < i; j++)
+    {
+        for(int k=j-1;k>-1;k--){
+                if( dir1[j]==dir1[k])
+                {
+                    og=1;
+                    break;
+                }
+            }
+            if(j==0)
+                cout<<dir1[j];
+            else if(og==1){
+                og=0;
+            }
+            else{
+                cout<<" "<<dir1[j];
+            }
+        
+    }
+    cout<<endl;    
+}
